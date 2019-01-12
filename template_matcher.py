a2f9338da3573fe79d723960459186bfc6258222
jpnaterer <jordan.naterer@gmail.com>
2019-01-11 22:25:34 -0800

    Fixed region zeroing

--- template_matcher.py
+++ template_matcher.py
@@ -30,7 +30,7 @@ def tm_test(capture, step_size, frame_range=None,
 
         # Get the confidence and location of cv2.matchTemplate().
         confidence_list, tl_list = get_tm_results(frame, template_img,
-            template_mask, num_results=4, roi_flag=roi_flag)
+            template_mask, roi_flag, num_results=4)
 
         # Display the frame with an accuracy label if show_flag is enabled.
         if show_flag:
@@ -42,8 +42,7 @@ def tm_test(capture, step_size, frame_range=None,
 # Return the confidence and location of the result of cv2.matchTemplate().
 # Parameters include num_results to specify the number of templates to search,
 # and an roi_flag to only search a sub-region of the frame.
-def get_tm_results(frame, template_img, template_mask, 
-    num_results, roi_flag=True):
+def get_tm_results(frame, template_img, template_mask, roi_flag, num_results):
 
     # Assuming a Wide 360p format (640×360), only search the bottom quarter
     # of the input frame for the template if the roi_flag is enabled.
@@ -54,20 +53,15 @@ def get_tm_results(frame, template_img, template_mask,
     match_mat = cv2.matchTemplate(
         frame, template_img, cv2.TM_CCORR_NORMED, mask=template_mask)
 
-    ### TODO: Comment
-    max_val_list, top_left_list = get_match_results(match_mat, num_results)
-    match_img = np.array(match_mat*255, dtype=np.uint8)
-    cv2.imshow("match", match_img)
-    cv2.waitKey(0)
-    ###
+    # Retrieve the confidence and top-left points from the match matrix.
+    confidence_list, tl_list = get_match_results(match_mat, num_results)
 
     # Compensate for point location if a region of interest was used.
     if roi_flag:
         for i in range(num_results):
-            top_left_list[i] = (top_left_list[i][0], top_left_list[i][1] + 270)
-        #top_left = (top_left[0], top_left[1] + 270)
+            tl_list[i] = (tl_list[i][0], tl_list[i][1] + 270)
 
-    return max_val_list, top_left_list
+    return confidence_list, tl_list
 
 
 # Take the result of cv2.matchTemplate, and find the n-most probable locations
@@ -87,14 +81,30 @@ def get_match_results(match_mat, num_results):
 # Take a matrix and coordinate, and set the region around that coordinate
 # to zeros. This function also prevents matrix out of bound errors if the
 # input coordinate is near the matrix border. Also, the input coordinate
-# is organized as (x, y) while matrices are organized (row, column). Matrices
+# is organized as (row, column) while matrices are organized (x, y). Matrices
 # are pass by reference, so the input can be directly modified.
-def set_subregion_to_zeros(input_mat, mat_dims, center_xy, radius):
-    tl = (max(center_xy[1]-radius, 0),
-          max(center_xy[0]-radius, 0))
-    br = (min(center_xy[1]+radius+1, mat_dims[0]-1),
-          min(center_xy[0]+radius+1, mat_dims[1]-1))
-    input_mat[tl[0]:br[0], tl[1]:br[1]] = np.zeros((radius*2+1, radius*2+1))
+def set_subregion_to_zeros(input_mat, mat_dims, center_pt, radius):
+
+    # Set the top-left and bot-right points of the zeroed region. Note that
+    # mat_dims is organized as (width, height) or (x-range,y-range).
+    tl = (max(center_pt[1]-radius, 0),
+          max(center_pt[0]-radius, 0))
+    br = (min(center_pt[1]+radius+1, mat_dims[0]-1),
+          min(center_pt[0]+radius+1, mat_dims[1]-1))
+
+    # Calculate the size of the region to be zeroed. Initialize it as a square
+    # of size (2r+1), then subtract off the region that is cutoff by a border.
+    x_size, y_size = radius*2+1, radius*2+1
+    if center_pt[1]-radius < 0:
+        x_size -= radius-center_pt[1]
+    elif center_pt[1]+radius+1 > mat_dims[0]-1:
+        x_size -= center_pt[1]+radius+1 - (mat_dims[0]-1)
+    if center_pt[0]-radius < 0:
+        y_size -= radius-center_pt[0]
+    elif center_pt[0]+radius+1 > mat_dims[1]-1:
+        y_size -= center_pt[0]+radius+1 - (mat_dims[1]-1)
+
+    input_mat[tl[0]:br[0], tl[1]:br[1]] = np.zeros((x_size, y_size))
 
 
 # Given an image location, extract the image and alpha (transparent) mask.
@@ -129,10 +139,13 @@ def get_image_and_mask(image_location, resize_ratio=None, gray_flag=False):
 
     return img, mask
 
-# TODO: Comment
+
+# Given a list of confidences and points, call the util.show_frame function
+# with the appropriate inputs. Operations include generating bottom-right
+# points and joining the multiple confidence values into a single string.
 def show_tm_results(frame, confidence_list, top_left_list, width, height):
     bbox_list = list()
-    for i, tl in enumerate(top_left_list):
+    for tl in top_left_list:
         br = (tl[0] + width, tl[1] + height)
         bbox_list.append((tl, br))
     label_list = ["{:0.3f}".format(i) for i in confidence_list]
