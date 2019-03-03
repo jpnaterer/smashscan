4f67c4883c63ed6c002665cbdcc3378920afa519
jpnaterer <jordan.naterer@gmail.com>
2019-03-02 20:15:20 -0800

    OCR Contour manipulation.

--- ocr.py
+++ ocr.py
@@ -1,6 +1,7 @@
 import time
 import cv2
 import pytesseract
+import numpy as np
 
 # SmashScan libraries
 import util
@@ -95,37 +96,69 @@ def display_ocr_test_flags(hsv_flag, avg_flag, gau_flag,
     print("inv_flag={}".format(inv_flag))
 
 
-capture = cv2.VideoCapture("videos/g6_1.mp4")
-frame = util.get_frame(capture, 1000)
+def contour_test(img):
+    _, contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    img_d = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(img_d, contours, -1, (255, 0, 0), 2)
+    cv2.imshow('test', img_d)
+    cv2.waitKey(0)
 
-img = cv2.imread('videos/test4.png', cv2.IMREAD_GRAYSCALE)
-show_ocr_result(img)
+    for i, contour in enumerate(contours):
+        img_d = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        cv2.drawContours(img_d, contour, -1, (255, 0, 0), 3)
 
-img2 = cv2.imread('videos/test4.png', cv2.IMREAD_COLOR)
-ocr_test(img2, hsv_flag=False)
-ocr_test(img2, hsv_flag=False, avg_flag=True)
-ocr_test(img2, hsv_flag=False, gau_flag=True)
-ocr_test(img2, hsv_flag=False, med_flag=True)
-ocr_test(img2, hsv_flag=False, bil_flag=True)
+        moment = cv2.moments(contour)
+        if moment['m00']: # Removes single points
+            cx = int(moment['m10']/moment['m00'])
+            cy = int(moment['m01']/moment['m00'])
+            print("Center: {}".format((cx, cy)))
+            cv2.circle(img_d, (cx, cy), 3, (0, 0, 255), -1)
 
-ocr_test(img2, hsv_flag=True)
-ocr_test(img2, hsv_flag=True, avg_flag=True)
-ocr_test(img2, hsv_flag=True, gau_flag=True)
-ocr_test(img2, hsv_flag=True, med_flag=True)
-ocr_test(img2, hsv_flag=True, bil_flag=True)
+        print("Area: {}".format(cv2.contourArea(contour)))
+        print("Permeter: {} ".format(cv2.arcLength(contour, True)))
 
-# https://docs.opencv.org/3.4.5/d7/d4d/tutorial_py_thresholding.html
-print("thresh")
-blur = cv2.GaussianBlur(img, (5, 5), 0)
-_, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
-thresh = cv2.medianBlur(thresh, 5)
-show_ocr_result(cv2.bitwise_not(thresh))
+        cv2.imshow('test', img_d)
+        cv2.waitKey(0)
 
-print("adaothresh")
-_, th4 = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-show_ocr_result(cv2.bitwise_not(th4))
+        mask = np.zeros(img.shape, np.uint8)
+        cv2.drawContours(mask, contours, i, 255, cv2.FILLED)
+        res = cv2.bitwise_and(img, img, mask=mask)
+        cv2.imshow('test', res)
+        cv2.waitKey(0)
 
-print("bluradaothresh")
-blur = cv2.GaussianBlur(img, (5, 5), 0)
-_, th5 = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU)
-show_ocr_result(cv2.bitwise_not(th5))
+
+for fnum in [3400, 5000]:
+    capture = cv2.VideoCapture("videos/tbh1.mp4")
+    frame = util.get_frame(capture, fnum, gray_flag=True)
+    frame = frame[300:340, 80:220]
+    cv2.imshow('frame', frame)
+    cv2.waitKey(0)
+
+    #frame = cv2.imread('videos/test4.png', cv2.IMREAD_GRAYSCALE)
+    #show_ocr_result(frame)
+
+    #img2 = cv2.imread('videos/test4.png', cv2.IMREAD_COLOR)
+    #ocr_test(img2, hsv_flag=False)
+    #ocr_test(img2, hsv_flag=False, avg_flag=True)
+    #ocr_test(img2, hsv_flag=False, gau_flag=True)
+    #ocr_test(img2, hsv_flag=False, med_flag=True)
+    #ocr_test(img2, hsv_flag=False, bil_flag=True)
+
+    #ocr_test(img2, hsv_flag=True)
+    #ocr_test(img2, hsv_flag=True, avg_flag=True)
+    #ocr_test(img2, hsv_flag=True, gau_flag=True)
+    #ocr_test(img2, hsv_flag=True, med_flag=True)
+    #ocr_test(img2, hsv_flag=True, bil_flag=True)
+
+    # https://docs.opencv.org/3.4.5/d7/d4d/tutorial_py_thresholding.html
+    print("thresh")
+    blur = cv2.GaussianBlur(frame, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+    th = cv2.medianBlur(thresh, 5)
+    show_ocr_result(cv2.bitwise_not(th))
+    contour_test(th)
+
+    print("adaothresh")
+    _, th2 = cv2.threshold(frame, 0, 255, cv2.THRESH_OTSU)
+    show_ocr_result(cv2.bitwise_not(th2))
